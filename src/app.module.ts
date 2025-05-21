bbd72adc5b4275245ba1440f7a0c126f7306464d
ThangNgo123 <ngosythang123456@gmail.com>
2025-05-21 10:13:39 +0700

    set up validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PingpongModule } from './pingpong/pingpong.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PingpongModule],
 })
 export class AppModule {}
---
+++ src/pingpong/pingpong.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('pingpong')
+export class PingpongController {}
